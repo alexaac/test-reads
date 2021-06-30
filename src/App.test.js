65d04f6bfdbc79a904e429db4ffa145ebf44bf31
Alexa Cristina <ac@mysite.org>
2021-06-30 12:17:15 +0300

    test: Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+
+test('renders MyReads', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/MyReads/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
